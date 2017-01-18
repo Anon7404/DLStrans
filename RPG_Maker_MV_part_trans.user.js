f9df4bf9c5bf02c4fcd31a5c933145b47a697cc4
Anon7404 <anon52790@gmail.com>
2017-01-19 00:26:44 +0300

    Update and rename Japanese_DLSite_translation.user.js to RPG_Maker_MV_part_trans.user.js

---
+++ RPG_Maker_MV_part_trans.user.js
@@ -0,0 +1,118 @@
+// ==UserScript==
+// @name           RPG Maker MV part trans
+// @namespace      http://userscripts.org/users/23652
+// @description    Replaces Japanese text for MV
+// @include        file://localhost/*.json
+// @grant          none
+// @copyright      JoeSimmons
+// @version        1.0
+// ==/UserScript==
+(function () {
+    'use strict';
+
+
+    /*
+        NOTE: 
+            You can use \\* to match actual asterisks instead of using it as a wildcard!
+            The examples below show a wildcard in use and a regular asterisk replacement.
+    */
+
+    var words = {
+    ///////////////////////////////////////////////////////
+
+
+        // Syntax: 'Search word' : 'Replace word',
+//game page
+        'ニューゲーム' : 'New Game',
+        'コンティニュー' : 'Continue',
+        'オプション' : 'Options',
+        'ファイル' : 'File',
+        'リリス' : 'Lilith',
+        'アイテム' : 'Items',
+        'スキル' : 'Skills',
+        '装備' : 'Equip',
+        'ステータス' : 'Status',
+        'セーブ' : 'Save',
+        'ゲーム終了' : 'Quit Game',
+        '攻撃力' : 'Attack',
+        '防御力' : 'Defence',
+        '魔法力' : 'Magic',
+        '魔法防御' : 'M.Def.',
+        '敏捷性' : 'Int.',
+        '運' : 'Luck',
+        //'' : '',
+
+
+      
+    ///////////////////////////////////////////////////////
+    '':''};
+
+
+
+
+
+
+
+
+
+
+
+    //////////////////////////////////////////////////////////////////////////////
+    // This is where the real code is
+    // Don't edit below this
+    //////////////////////////////////////////////////////////////////////////////
+
+    var regexs = [], replacements = [],
+        tagsWhitelist = ['INPUT', 'TEXTAREA'],
+        rIsRegexp = /^\/(.+)\/([gim]+)?$/,
+        word, text, texts, i, userRegexp;
+
+    // prepareRegex by JoeSimmons
+    // used to take a string and ready it for use in new RegExp()
+    function prepareRegex(string) {
+        return string.replace(/([\[\]\^\&\$\.\(\)\?\/\\\+\{\}\|])/g, '\\$1');
+    }
+
+    // function to decide whether a parent tag will have its text replaced or not
+    function isTagOk(tag) {
+        return tagsWhitelist.indexOf(tag) === -1;
+    }
+
+    delete words['']; // so the user can add each entry ending with a comma,
+                      // I put an extra empty key/value pair in the object.
+                      // so we need to remove it before continuing
+
+    // convert the 'words' JSON object to an Array
+    for (word in words) {
+        if ( typeof word === 'string' && words.hasOwnProperty(word) ) {
+            userRegexp = word.match(rIsRegexp);
+
+            // add the search/needle/query
+            if (userRegexp) {
+                regexs.push(
+                    new RegExp(userRegexp[1], 'g')
+                );
+            } else {
+                regexs.push(
+                    new RegExp(prepareRegex(word).replace(/\\?\*/g, function (fullMatch) {
+                        return fullMatch === '\\*' ? '*' : '[^ ]*';
+                    }), 'g')
+                );
+            }
+
+            // add the replacement
+            replacements.push( words[word] );
+        }
+    }
+
+    // do the replacement
+    texts = document.evaluate('//body//text()[ normalize-space(.) != "" ]', document, null, 6, null);
+    for (i = 0; text = texts.snapshotItem(i); i += 1) {
+        if ( isTagOk(text.parentNode.tagName) ) {
+            regexs.forEach(function (value, index) {
+                text.data = text.data.replace( value, replacements[index] );
+            });
+        }
+    }
+
+}());
